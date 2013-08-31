8a0bb1fa0ab36fd35810dad3bc09aef95b3436b6
Speedy Gonzales <vahid.hedayati@gmail.com>
2013-08-31 21:11:36 +0100

    Update ExtLinkChoiceTagLib.groovy

--- grails-app/taglib/taglib/extlinkchoice/ExtLinkChoiceTagLib.groovy
+++ grails-app/taglib/taglib/extlinkchoice/ExtLinkChoiceTagLib.groovy
@@ -11,22 +11,39 @@ class ExtLinkChoiceTagLib {
 		def description = attrs.remove('description')?.toString()
 		def modalLabel  = attrs.remove('modalLabel')?.toString()
 		def resolveit=attrs.remove('resolveit')?.toString()
+		
 		if (!modalLabel) modalLabel='Example Link'
 		int resolv=0
 		if ((resolveit!=null) && (resolveit.matches("[0-9]+"))) { resolv=Integer.parseInt(resolveit) } 
 		if(link  && description) {
 			def result = extLinkChoiceService.returnLink(link,description,resolv,modalLabel)
 			out << "${result}"
-			
-			
 		}
 	}
 	
-
+	def modalFooter= {
+		out << "<div class='modal hide fade' id='myModal' tabindex='-1' role='dialog' aria-labelledby='myModalLabel' aria-hidden='true'>\n"
+		out << "<div class='modal-header'>\n"
+		out << "<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button>\n"
+		out << "<div id='myModalLabel'><h3></h3></div>\n"
+		out << "</div>\n"
+		out << "<div class='modal-body'>\n"
+		out << "</div>\n"
+		out << "</div>\n"
+		out << "<script type='text/javascript'>\n"
+		out << "\$('a.btn').on('click', function(e) {\n"
+		out << "e.preventDefault();\n"
+		out << "var url = \$(this).attr('href');\n"
+		out << "\$(\".modal-body\").html('<iframe width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\" allowtransparency=\"true\" src=\"'+url+'\"></iframe>');\n"
+		out << "});\n"
+		out << "</script>\n"	
+	}
+	
 	def userPref = { attrs, body ->
 		def update = attrs.remove('update')?.toString()
 		def updateShow = attrs.remove('updateShow')?.toString()
-		if (params.linkchoice!=null) { session.linkchoice=params.linkchoice; }
+		if (params.linkchoice!=null) session.linkchoice=params.linkchoice
+		if (session.linkchoice==null) session.linkchoice='_same'
 		out << "<script type='text/javascript'>\n"
 		out << " \$(document).ready(function() {\n"
 		out << "\$('#" + update+"').hide();\n"
@@ -35,10 +52,10 @@ class ExtLinkChoiceTagLib {
 		out << "\$('#" + update+"').slideToggle();"
 		out << "});\n"
 		out << "var appendit='Link Open type:  - (${session.linkchoice }) - <br/>';\n"
-		out << "appendit +='<a href=?linkchoice=_modal>Pop up Modal</a><br/>';\n"
+		out << "appendit +='<a href=?linkchoice=_modal>Modal: Pop-up</a><br/>';\n"
 		out << "appendit +='<a href=?linkchoice=_same>Same Window</a><br/>';\n"
 		out << "appendit +='<a href=?linkchoice=_new>New Window</a><br/>';\n"
-		out << "appendit +='<a href=?linkchoice=_multi>multiplechoice</a><br/>';\n"
+		out << "appendit +='<a href=?linkchoice=_multi>Multiple Choice</a><br/>';\n"
 		out << "\$('#" + update+"').html(appendit);\n"
 		out << "});\n"
 		out << "</script>"
