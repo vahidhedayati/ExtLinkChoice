f7c5c71393840af307a9ad3d12390c047edf3ffb
Vahid H <vahid.hedayati@gmail.com>
2014-02-07 17:02:35 +0000

    added bootstrapfooter

--- grails-app/taglib/extlinkchoice/ExtLinkChoiceTagLib.groovy
+++ grails-app/taglib/extlinkchoice/ExtLinkChoiceTagLib.groovy
@@ -78,6 +78,33 @@ class ExtLinkChoiceTagLib {
 		out << "</script>\n"	
 	}
 	
+	 def modalbootstrapFooter= {
+
+                out << """<div class="modal  fade" id="myModal" role="dialog"  aria-hidden="true" >
+                <div class="modal-dialog">
+                <div class="modal-content"  style="width:60em;height:50em;overflow:auto;">
+                <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">x</button>
+                <h3></h3>
+                </div>
+
+                <div class="modal-body"  style="width:100%;height:100%; ">
+
+                </div></div></div></div>"""
+
+
+                out << "<script type='text/javascript'>\n"
+                out << "\$('a.btn').on('click', function(e) {\n"
+                out << "e.preventDefault();\n"
+                out << "var url = \$(this).attr('href');\n"
+                out << "\$(\".modal-body\").html('<iframe width=\"100%\" height=\"100%\" frameborder=\"0\" scrolling=\"no\" allowtransparency=\"false\" src=\"'+url+'\"></iframe>');\n"
+                out << "});\n"
+                out << "</script>\n"
+
+        }
+
+
+
 	def userPref = { attrs, body ->
 		def update = attrs.remove('update')?.toString()
 		def updateShow = attrs.remove('updateShow')?.toString()
